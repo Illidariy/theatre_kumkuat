d778b13bc923dd95fa09df428903f1a591378af6
Nickolay-Klimentov <moody.klmn@icloud.com>
2023-02-17 16:47:22 +0300

    fix merge problems

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ActorsList from '../features/actors/ActorsList';
 import PerfomancesList from '../features/perfomances/PerfomancesList';
-import Registration from '../features/auth/Registration';
+// import Registration from '../features/auth/Registration';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Routes>
         <Route path="/perfomances" element={<PerfomancesList />} />
-        {/* <header className="App-header" /> */}
         <Route path="/actors" element={<ActorsList />} />
-        <header className="App-header" />
-        <Registration />
-        <a href="#twf">Накупить билетов</a>
+        {/* <Registration /> */}
+        {/* <a href="#twf">Накупить билетов</a> */}
       </Routes>
     </div>
   );
