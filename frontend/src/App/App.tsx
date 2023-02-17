a48d0430cd58e9e51d81591155e852e977f18955
PenRomain <romandreevich@icloud.com>
2023-02-17 16:09:13 +0300

    timepad-done prettier

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header" />
+
+      <a href="#twf">Накупить билетов</a>
     </div>
   );
 }
