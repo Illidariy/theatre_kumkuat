17ad393a46502f6445f20fa1ed2a59c004a67e5b
Ilia <sorockiniliuh@yandex.ru>
2023-02-20 19:43:19 +0300

    doing contacts

--- frontend/src/features/Contacts/ContactsPage.tsx
+++ frontend/src/features/Contacts/ContactsPage.tsx
@@ -1,32 +1,25 @@
 import React from 'react';
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
-const mapState = { center: [55.76, 37.64], zoom: 10 };
-
 function ContactsPage(): JSX.Element {
   return (
-    // <YMaps>
-    //   <div className="contacts">
-    //     {/* <div className="container contacts__container" /> */}
-    //     <Map
-    //       defaultState={{
-    //         center: [59.911597, 30.267431],
-    //         zoom: 9,
-    //       }}
-    //     />
-    //     <Placemark geometry={[59.911597, 30.267431]} />
-    //   </div>
-    // </YMaps>
-    <YMaps>
-      <Map
-        defaultState={{
-          center: [59.911597, 30.267431],
-          zoom: 12,
-        }}
-      >
-        <Placemark geometry={[59.911597, 30.267431]} />
-      </Map>
-    </YMaps>
+    <div className="contacts">
+      <div className="container contacts__container">
+        <div className="contacts__flex">
+          <div className="contacts__item"></div>
+          <YMaps>
+            <Map
+              defaultState={{
+                center: [59.911597, 30.267431],
+                zoom: 12,
+              }}
+            >
+              <Placemark geometry={[59.911597, 30.267431]} />
+            </Map>
+          </YMaps>
+        </div>
+      </div>
+    </div>
   );
 }
 
