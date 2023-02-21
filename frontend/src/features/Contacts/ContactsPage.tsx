799a81114bba48c0e87f17c0437a569fd23d9617
Ilia <sorockiniliuh@yandex.ru>
2023-02-21 16:11:52 +0300

    ways done,need to style

--- frontend/src/features/Contacts/ContactsPage.tsx
+++ frontend/src/features/Contacts/ContactsPage.tsx
@@ -81,49 +81,49 @@ function ContactsPage(): JSX.Element {
             <p>43 автобус остановка ул.Степана Разина</p>
             <p>67 автобус остановка ул.Лифляндская</p>
             <div />
+          </div>
+          <br />
+          <div className="contacts__numbers">
+            <h4>По всем вопросам</h4>
+            <p>Александра (тел. +7 931-531-92-62)</p>
             <br />
-            <div className="contacts__numbers">
-              <h4>По всем вопросам</h4>
-              <p>Александра (тел. +7 931-531-92-62)</p>
-              <br />
-              <h4>PR, работа с партнёрами</h4>
-              <p>Евгения, Денис (тел. +7 921-391-93-98)</p>
-              <br />
-              <h4>Наш адрес</h4>
-              <p>г. Санкт-Петербург, ст.м. "Нарвская", ул. Курляндская, 49</p>
-              <br />
-              <h4>Мы в Интернете</h4>
-              <br />
-              <div className="contacts__items">
-                <a href="https://vk.com/teatr_kumkuat">
-                  <img
-                    src="../../images/vkontacte.png"
-                    alt="vk"
-                    className="nav__a-size"
-                  />
-                </a>
-                <a href="https://t.me/teatr_kumkuat">
-                  <img
-                    src="../../images/telegram.png"
-                    alt="telegram"
-                    className="nav__a-size"
-                  />
-                </a>
-                <a href="https://twitter.com/teatr_kumkuat">
-                  <img
-                    src="../../images/twit.png"
-                    alt="twitter"
-                    className="nav__a-size"
-                  />
-                </a>
-                <a href="https://www.youtube.com/user/teatrkumkuat">
-                  <img
-                    src="../../images/youtube2.png"
-                    alt="youtube"
-                    className="nav__a-size"
-                  />
-                </a>
-              </div>
+            <h4>PR, работа с партнёрами</h4>
+            <p>Евгения, Денис (тел. +7 921-391-93-98)</p>
+            <br />
+            <h4>Наш адрес</h4>
+            <p>г. Санкт-Петербург, ст.м. "Нарвская", ул. Курляндская, 49</p>
+            <br />
+            <h4>Мы в Интернете</h4>
+            <br />
+            <div className="contacts__items">
+              <a href="https://vk.com/teatr_kumkuat">
+                <img
+                  src="../../images/vkontacte.png"
+                  alt="vk"
+                  className="nav__a-size"
+                />
+              </a>
+              <a href="https://t.me/teatr_kumkuat">
+                <img
+                  src="../../images/telegram.png"
+                  alt="telegram"
+                  className="nav__a-size"
+                />
+              </a>
+              <a href="https://twitter.com/teatr_kumkuat">
+                <img
+                  src="../../images/twit.png"
+                  alt="twitter"
+                  className="nav__a-size"
+                />
+              </a>
+              <a href="https://www.youtube.com/user/teatrkumkuat">
+                <img
+                  src="../../images/youtube2.png"
+                  alt="youtube"
+                  className="nav__a-size"
+                />
+              </a>
             </div>
           </div>
         </div>
