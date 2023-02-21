84cca81b1cbc2412ffef20fa631730c300a2354a
Tema <rzhevka7@gmail.com>
2023-02-21 11:33:46 +0300

    spectacles with nav

--- frontend/src/features/SpectaclesPage/Types/types.ts
+++ frontend/src/features/SpectaclesPage/Types/types.ts
@@ -14,3 +14,27 @@ export type State = {
 };
 
 export type SpectacleId = Spectacle['id'];
+
+export type SpectacleInfoList = SpectacleInfo[];
+
+export type SpectacleInfo = {
+  id?: number;
+  title: string;
+  body: string;
+  isActual: boolean;
+  mainPhoto: string;
+  video: string;
+  directorId?: number;
+  'Director.id':number;
+  'Director.firstName': string;
+  'Director.secondName': string;
+  'Director.mainPhoto': string;
+  'Director.title': string;
+  'Director.body':string;
+  'actors.id': number;
+  'actors.firstName': string;
+  'actors.secondName': string;
+  'actors.mainPhoto': string;
+  'actors.title': string;
+  'actors.body': string;
+};
