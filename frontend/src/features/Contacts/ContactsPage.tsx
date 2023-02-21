83b0b12b74b8e7b8e1a3b6245ff70a9cb6dc06f7
Ilia <sorockiniliuh@yandex.ru>
2023-02-21 17:38:41 +0300

    contacts page is done

--- frontend/src/features/Contacts/ContactsPage.tsx
+++ frontend/src/features/Contacts/ContactsPage.tsx
@@ -5,84 +5,8 @@ import './Contacts.scss';
 function ContactsPage(): JSX.Element {
   return (
     <div className="contacts">
-      <div className="container contacts__container">
-        <div className="contacts__flex">
-          <div className="map">
-            <YMaps>
-              <Map
-                defaultState={{
-                  center: [59.911597, 30.267431],
-                  zoom: 12,
-                }}
-                width="400px"
-                height="400px"
-              >
-                <Placemark geometry={[59.911597, 30.267431]} />
-              </Map>
-            </YMaps>
-          </div>
-        </div>
+      <div className="container">
         <div className="contacts__text">
-          <div className="contacts__way">
-            <h1>
-              <img
-                src="../../images/placemark.png"
-                alt="placemark"
-                className="nav__a-size"
-              />
-              Как добраться
-            </h1>
-            <h4>
-              <img
-                src="../../images/bus.png"
-                alt="bus"
-                className="nav__a-size"
-              />
-              От метро Технологический Институт,Сенная
-            </h4>
-            <p>71 автобус остановка ул.Степана Разина</p>
-            <br />
-            <h4>
-              <img
-                src="../../images/bus.png"
-                alt="bus"
-                className="nav__a-size"
-              />
-              От метро Нарвская
-            </h4>
-            <p>66 автобус остановка ул.Степана Разина</p>
-            <p>6 автобус остановка ул.Курляндская</p>
-            <h4>
-              <img
-                src="../../images/bus.png"
-                alt="bus"
-                className="nav__a-size"
-              />
-              От метро Звенигородская
-            </h4>
-            <p>290 автобус остановка ул.Степана Разина</p>
-            <h4>
-              <img
-                src="../../images/bus.png"
-                alt="bus"
-                className="nav__a-size"
-              />
-              От метро Чернышевская, Гостиный двор
-            </h4>
-            <p>22 автобус остановка ул.Степана Разина</p>
-            <h4>
-              <img
-                src="../../images/bus.png"
-                alt="bus"
-                className="nav__a-size"
-              />
-              От метро Балтийская
-            </h4>
-            <p>43 автобус остановка ул.Степана Разина</p>
-            <p>67 автобус остановка ул.Лифляндская</p>
-            <div />
-          </div>
-          <br />
           <div className="contacts__numbers">
             <h4>По всем вопросам</h4>
             <p>Александра (тел. +7 931-531-92-62)</p>
@@ -124,9 +48,88 @@ function ContactsPage(): JSX.Element {
                   className="nav__a-size"
                 />
               </a>
+              <br />
+              <br />
             </div>
           </div>
+          <div className="contacts__way">
+            <div className="contacts__way-text">
+              <img
+                src="../../images/placemark.png"
+                alt="placemark"
+                className="nav__a-size"
+              />
+              <h1>Как добраться</h1>
+            </div>
+            <br />
+            <div className="contacts__way-text">
+              <img
+                src="../../images/bus.png"
+                alt="bus"
+                className="nav__a-size"
+              />
+              <h4>От метро Технологический Институт,Сенная</h4>
+            </div>
+            <p>71 автобус остановка ул.Степана Разина</p>
+            <br />
+            <div className="contacts__way-text">
+              <img
+                src="../../images/bus.png"
+                alt="bus"
+                className="nav__a-size"
+              />
+              <h4>От метро Нарвская</h4>
+            </div>
+            <p>66 автобус остановка ул.Степана Разина</p>
+            <p>6 автобус остановка ул.Курляндская</p>
+            <br />
+            <div className="contacts__way-text">
+              <img
+                src="../../images/bus.png"
+                alt="bus"
+                className="nav__a-size"
+              />
+              <h4>От метро Звенигородская</h4>
+            </div>
+            <p>290 автобус остановка ул.Степана Разина</p>
+            <br />
+            <div className="contacts__way-text">
+              <img
+                src="../../images/bus.png"
+                alt="bus"
+                className="nav__a-size"
+              />
+              <h4>От метро Чернышевская, Гостиный двор</h4>
+            </div>
+            <p>22 автобус остановка ул.Степана Разина</p>
+            <br />
+            <div className="contacts__way-text">
+              <img
+                src="../../images/bus.png"
+                alt="bus"
+                className="nav__a-size"
+              />
+              <h4>От метро Балтийская</h4>
+            </div>
+            <p>43 автобус остановка ул.Степана Разина</p>
+            <p>67 автобус остановка ул.Лифляндская</p>
+            <div />
+          </div>
         </div>
+      </div>
+      <div className="map">
+        <YMaps>
+          <Map
+            defaultState={{
+              center: [59.911597, 30.267431],
+              zoom: 12,
+            }}
+            width="100%"
+            height="400px"
+          >
+            <Placemark geometry={[59.911597, 30.267431]} />
+          </Map>
+        </YMaps>
       </div>
     </div>
   );
